04c7e9739fe0abb9d3222da8e369fadc509202a5
alibechegg <83501490+alibechegg@users.noreply.github.com>
2021-05-03 19:25:43 +0600

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<!-- jQuery -->
-<script src="/demo(phpispan)/CmsPro(Alibek)/js/jquery.js"></script>
-
-<!-- Bootstrap Core JavaScript -->
-<script src="/demo(phpispan)/CmsPro(Alibek)/js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
